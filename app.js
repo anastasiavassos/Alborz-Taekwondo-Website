6692d79ee174e591855e1711c898fe072e09accc
Anastasia Vassos <97248510+anastasiavassos@users.noreply.github.com>
2023-05-01 15:26:06 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+/*let yellowbtn=document.querySelector("button");
+let greenbtn=document.querySelector("button");
+let bluebtn=document.querySelector("button");
+let redbtn=document.querySelector("button");
+let blackbtn = querySelector("button");
+var belt_img = document.getElementsById("belt_img");
+
+
+function show_yellow() { 
+    belt_img.src = "yellow_belt.jpg";
+    document.getElementById("belt_heading").innerHTML="Yellow Belt";
+    document.getElementById("belt_para").innerText ="This program is designed to introduce the";
+    return false;
+
+} 
+function show_green() { 
+    belt_img.src = "green_belt.jpg"; 
+    document.getElementbyId("belt_heading").innerHTML="Green Belt";
+    return false;
+} 
+
+function show_blue() { 
+    belt_img.src = "blue_belt.jpg";
+    document.getElementById("belt_heading").innerHTML="Blue Belt";
+    return false;
+}  
+function show_red() { 
+    document.getElementById("belt_heading").innerHTML="Red Belt";
+    img.src = "red_belt.jpg";
+    return false;
+}  
+function show_black() { 
+    document.getElementById("belt_heading").innerHTML="Black Belt";
+    img.src = "black_belt.jpg"; 
+    return false;
+} 
+
+/*document.getElementById("yellowbtn").addEventListener('click', show_yellow);
+document.getElementById("greenbtn").addEventListener("click", show_green);
+document.getElementById("bluebtn").addEventListener("click", show_blue);
+document.getElementById("redbtn").addEventListener("click", show_red);
+document.getElementById("blackbtn").addEventListener("click", show_black);*/
+
